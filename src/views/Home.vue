bb18491d8bc81054cb7424950fdf31483eece8bc
Channa Ly <channa.info@gmail.com>
2021-10-12 23:17:39 +0700

    Close #3 create a first vue component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <item-list :items='items' />
+    <hello-world msg="Welcome to Your Vue.js + TypeScript App"/>
+
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import ItemList from '@/components/ItemList.component.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
+    ItemList
   },
+
+  setup(){
+    const items: any[] = [
+      {id: 1, name: 'Ruby'},
+      {id: 2, name: 'Php'},
+      {id: 3, name: 'Python'},
+      {id: 4, name: 'C'},
+    ]
+
+    return {items};
+  }
 });
 </script>
